d7d6222a96b64c2a8030315c69d2843a7ff5d845
EFluet <etienne.fluet@gmail.com>
2019-10-14 11:55:21 -0700

    organizing folders

--- runall.r
+++ runall.r
@@ -1,40 +1,60 @@
 # /----------------------------------------------------------------------------#
-#/ Calculate a CH4 flux normalized to temperature & NEE, so that all the variation left is from spatial coverage.
-#  Authors: Mathias Goedecke & Etienne Fluet-Chouinard, 2019
+#/  Powell WG3 
+#   Calculate a CH4 flux normalized to temperature & NEE, 
+#   so that all the variation left is from spatial coverage.
 
-# Make stacked cumulative plots for all towers.
-# Run clustering on flux & direction. See how mny clusters is optimal.
-# Make wind rose of the frequency of each' stacked barplot..
+#   Authors: Mathias Goedecke & Etienne Fluet-Chouinard, 2019
+#   Institutions: Max Plank Inst. Jena;  Stanford University
 
-# IS THERE AN ASSOCIATION BETWEEN 
-# IS THERE AN ASSOCIATION BETWEEN SEASONALITY AND FLUX-DIRECTION?
-# Calculate cumulative flux per direction? 
+#   Make stacked cumulative plots for all towers.
+#   Run clustering on flux & direction. See how mny clusters is optimal.
+#   Make wind rose of the frequency of each' stacked barplot..
 
+#   Questions: 
+#    - Is flux direction at each tower (non-)uniformly distributed?
+#    - Is flux intensity related to wind direction? Across seasons or diurnal cycle?
+
+
+
+# /----------------------------------------------------------------------------#
+#/   Import libraries & themes
 library(here)
 here()
 
-source("scripts/import_libraries.r") 
-source("scripts/line_plot_theme.r") 
+source("prep/import_libraries.r") 
+source("plot/line_plot_theme.r") 
 
 
-# Read and bind files from each site
-source("scripts/compile_tower_flux.r")
+# /----------------------------------------------------------------------------#
+#/ Read flux data from each site and bind into one df.
+source("prep/compile_tower_flux.r")
+
+
+# /----------------------------------------------------------------------------#
+#/  First generation plots
+
 
 # Calculate cumulative freq across wind sectors, and plot it
-source("scripts/cumulative_sector_plots.r")
+source("plot/cumulative_sector_plots.r")
 
 # Make windrose plots: by sectors & with point density
-source("scripts/windrose_plot.r") 
+source("plot/windrose_plot.r") 
 
 # Cluster fluxes  & plot on windrose
-source("scripts/wd_flux_clustering.r") 
+source("proc/wd_flux_clustering.r") 
+
+
+# /----------------------------------------------------------------------------#
+#/  Circular statistics
 
 
 # Linear-circular correlation of fluxes and wind direction (as polar coordiantes)
+source("proc/wd_correlation.r") 
+
 # TODO: Some ARMA time-series applied to the lin-circ model. 
-source("scripts/wd_correlation.r") 
 
 
-source("scripts/temporal_avg_dir.r") 
+# Average flux & WF per month, diurnal phase.
+source("proc/temporal_avg_dir.r") 
 
 
